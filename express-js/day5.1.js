e905158a75aef922874501da881ff188471e7d50
missmystique-ux <minepalwasha@gmail.com>
2026-06-26 15:53:36 +0500

    express-js

---
+++ express-js/day5.1.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact me.');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Hello blog');
+});
+
+app.get('/blog/:slug', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send(`Hello ${req.params.slug}`);
+});
+
+// app.get('/blog/:slug/:second', (req, res) => {
+//     res.send(`Hello ${req.params.slug} and ${req.params.second}`);
+// });
+
+// app.get('/blog/intro-to-js', (req, res) => {
+//   res.send('Hello intro-to-js');
+// });
+
+// app.get('/blog/intro-to-python', (req, res) => {
+//   res.send('Hello intro-to-python');
+// });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
